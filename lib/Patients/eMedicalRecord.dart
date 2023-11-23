5ab7c06fceca52b5cdc27519a6d475dd82ae4b32
Nur Atiqa Idayu <113422395+littlequiorra@users.noreply.github.com>
2023-11-23 13:43:44 +0800

    Update eMedicalRecord.dart

--- lib/Patients/eMedicalRecord.dart
+++ lib/Patients/eMedicalRecord.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -45,35 +46,75 @@ class _eMedicalRecordScreenState extends State<eMedicalRecordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
+      toolbarHeight: 98,
       backgroundColor: Colors.white,
-      title: Center(child: Text("MyTeleClinic",
-        style: const TextStyle(fontSize: 32, color:Colors.blueAccent),)
+      title: Center(child: Image.asset(
+        "asset/MYTeleClinic.png",
+        width: 394,
+        height: 258,
+      ),
       ),
     ),
       body: Container(
         padding: EdgeInsets.all(7),
         child: Column(
             children: [
-              Text("Vital Info",
-                style: const TextStyle(fontSize: 32, color:Colors.black),), //<------------
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Text("Vital Info",
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight. bold,
+                    textStyle: const TextStyle(fontSize: 32, color:Colors.black),),),
+              ), //<------------
               Expanded(
-                child:GridView.builder(
-                  itemCount: _infos.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount:3),
-                  itemBuilder: (context,index){
-                    return ListTile(
-                      title: Image.network(_infos[index]["Image"]!,
-                        width: 100,height: 100,),
-                      subtitle: Column(
-                        children: [
-                          Text(_infos[index]["Info"]!,
-                            style: const TextStyle(fontSize: 24, color:Colors.black),),
-                        ],
+                child:SizedBox(
+                  width: 874,
+                  height: 1.0,
+                  child: Container(
+                    margin: EdgeInsets.all(24),
+                    padding: EdgeInsets.only(top: 18),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blueAccent),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(24.0) ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blueGrey,
+                          offset: const Offset(
+                            5.0,
+                            5.0,
+                          ),
+                          blurRadius: 10.0,
+                          spreadRadius: 2.0,
+                        ), //BoxShadow
+                        BoxShadow(
+                          color: Colors.white,
+                          offset: const Offset(0.0, 0.0),
+                          blurRadius: 0.0,
+                          spreadRadius: 0.0,
+                        ), //BoxShadow
+                      ],//                 <--- border radius here
                       ),
+                    child: GridView.builder(
+                      itemCount: _infos.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount:3),
+                      itemBuilder: (context,index){
+                        return ListTile(
+                          title: Image.network(_infos[index]["Image"]!,
+                            width: 100,height: 100,),
+                          subtitle: Column(
+                            children: [
+                              Text(_infos[index]["Info"]!,
+                                style: const TextStyle(fontSize: 24, color:Colors.black),),
+                            ],
+                          ),
 
-                    );
-                  }, ),
+                        );
+                      }, ),
+                  ),
+                ),
               ),
             ]),
       ),
