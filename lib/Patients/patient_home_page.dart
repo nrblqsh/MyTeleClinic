2f05888bc6660b7d4b9ed364d5b66e304492ef42
Nur Atiqa Idayu <113422395+littlequiorra@users.noreply.github.com>
2023-12-14 10:11:56 +0800

    Update patient_home_page.dart

--- lib/Patients/patient_home_page.dart
+++ lib/Patients/patient_home_page.dart
@@ -249,7 +249,7 @@ class _HomePageState extends State<HomePage> {
                       context,
                       MaterialPageRoute(
                           builder: (context) =>
-                          CurrentVitalInfoScreen(patientID: patientID,)),
+                          ViewAppointmentScreen(patientID: patientID,)),
                     );
                   },
                   child: Column(
